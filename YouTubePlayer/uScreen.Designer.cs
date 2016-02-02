a20b3ce5ebbe4fb776b996153e5e561e0fdb2866
sthakuri <suvashshah.thakuri@gmail.com>
2016-02-01 16:28:12 -0800

    initial commit

---
+++ YouTubePlayer/uScreen.Designer.cs
@@ -0,0 +1,69 @@
+﻿namespace YouTubePlayer
+{
+    partial class uScreen
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(uScreen));
+            this.axShockwaveFlash1 = new AxShockwaveFlashObjects.AxShockwaveFlash();
+            ((System.ComponentModel.ISupportInitialize)(this.axShockwaveFlash1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // axShockwaveFlash1
+            // 
+            this.axShockwaveFlash1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.axShockwaveFlash1.Enabled = true;
+            this.axShockwaveFlash1.Location = new System.Drawing.Point(0, 0);
+            this.axShockwaveFlash1.Name = "axShockwaveFlash1";
+            this.axShockwaveFlash1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axShockwaveFlash1.OcxState")));
+            this.axShockwaveFlash1.Size = new System.Drawing.Size(230, 149);
+            this.axShockwaveFlash1.TabIndex = 0;
+            this.axShockwaveFlash1.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.axShockwaveFlash1_PreviewKeyDown);
+            // 
+            // uScreen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(230, 149);
+            this.Controls.Add(this.axShockwaveFlash1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.Name = "uScreen";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "U Player";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.uScreen_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.uScreen_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.axShockwaveFlash1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private AxShockwaveFlashObjects.AxShockwaveFlash axShockwaveFlash1;
+    }
+}
---
+++ YouTubePlayer/uScreen.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace YouTubePlayer
+{
+    public partial class uScreen : Form
+    {
+        public uScreen()
+        {
+            InitializeComponent();
+        }
+
+        private void uScreen_Load(object sender, EventArgs e)
+        {
+            
+        }
+        
+        private void uScreen_KeyDown(object sender, KeyEventArgs e)
+        {
+           
+            if (e.Control && e.KeyCode == Keys.V)
+            {
+                PlayNewChannel();
+            }
+        }
+
+        private void axShockwaveFlash1_PreviewKeyDown(object sender, PreviewKeyDownEventArgs e)
+        {
+            if (e.Control && e.KeyCode == Keys.V)
+            {
+                PlayNewChannel();
+            }
+        }
+
+        private void PlayNewChannel()
+        {
+            string url = Clipboard.GetText();
+            if (url.Contains(("youtube.com")))
+            {
+                string token = url.Remove(0, url.IndexOf("=") + 1);
+
+                this.axShockwaveFlash1.Movie = string.Format("https://www.youtube.com/v/{0}", token);
+                this.axShockwaveFlash1.Play();
+            }
+        }
+    }
+}
